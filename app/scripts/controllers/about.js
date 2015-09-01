c473f87a0f612439efb7a09f6498a8c3063573dd
user <user@debian-lenovo>
2015-09-01 11:15:09 -0500

    install fresh

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name postsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the postsApp
+ */
+angular.module('postsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name postsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the postsApp
+ */
+angular.module('postsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
